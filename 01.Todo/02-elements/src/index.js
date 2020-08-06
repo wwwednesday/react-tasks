e99d15891b76190684029ac11fd35fd3ecf92ba1
yana-coderiver <yana@coderiver.com.ua>
2020-08-06 22:35:36 +0300

    features/Elements: Todo list layout

--- 01.Todo/02-elements/src/index.js
+++ 01.Todo/02-elements/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h2>Hello, world!</h2>;
+const el = (
+  <div>
+    <h2>Todo List</h2>
+    <input type="text" placeholder="Search" />
+    <ul>
+      <li>First item</li>
+      <li>Last item</li>
+    </ul>
+  </div>
+);
 
 ReactDOM.render(
   el,
