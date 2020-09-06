10260713235364304ea404cba10a5dd4af42bd3d
wwwednesday <yana@coderiver.com.ua>
2020-09-07 00:15:59 +0300

    Services created

---
+++ 02.Server/star-db/src/services/swapi-service.js
@@ -0,0 +1,34 @@
+export default class SwapiService {
+  _apiBase = 'https://swapi.dev/api';
+
+  async getResourse(url) {
+    const res = await fetch(`${this._apiBase}${url}`);
+    if (!res.ok) throw new Error (`Could not fetch ${this._apiBase}${url}, received ${res.status}`);
+
+    return await res.json();
+  }
+
+  getAllPeople() {
+    return this.getResourse('/people');
+  }
+
+  getPerson(id) {
+    return this.getResourse(`/people/${id}`);
+  }
+
+  getAllPlanets() {
+    return this.getResourse('/planets');
+  }
+
+  getPlanet(id) {
+    return this.getResourse(`/planets/${id}`);
+  }
+
+  getAllStarships() {
+    return this.getResourse('/starships');
+  }
+
+  getStarship(id) {
+    return this.getResourse(`/starships/${id}`);
+  }
+}
